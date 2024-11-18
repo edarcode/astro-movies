6b7d2dc200a08b787290ca8e9781ec7630c15f7f
Edwin Ortiz <edarcode@gmail.com>
2024-11-18 18:34:53 -0500

    api crud funcional

--- src/pages/api/movies/[id].ts
+++ src/pages/api/movies/[id].ts
@@ -1,17 +1,43 @@
 import type { APIRoute } from "astro";
+import { Res } from "../response/Res";
+import { db, eq, Movies } from "astro:db";
 
 export const prerender = false;
 
-export const PATCH: APIRoute = async ({ params: id }) => {
-  return new Response(JSON.stringify({ msg: "PATCH", id }), {
-    status: 201,
-    headers: { "Content-Type": "application/json" },
-  });
+export const PATCH: APIRoute = async ({ params, request }) => {
+  try {
+    const movieId = params.id as string;
+    const body = await request.json();
+
+    const result = await db
+      .update(Movies)
+      .set({ ...body })
+      .where(eq(Movies.id, movieId));
+
+    if (!result.rowsAffected) return Res.notFound();
+
+    return Res.json({
+      status: 201,
+      data: "Pélicula actualizada correctamente",
+    });
+  } catch (error) {
+    return Res.err();
+  }
 };
 
-export const DELETE: APIRoute = async ({ params: id }) => {
-  return new Response(JSON.stringify({ msg: "DELETE", id }), {
-    status: 201,
-    headers: { "Content-Type": "application/json" },
-  });
+export const DELETE: APIRoute = async ({ params }) => {
+  try {
+    const movieId = params.id as string;
+
+    const result = await db.delete(Movies).where(eq(Movies.id, movieId));
+
+    if (!result.rowsAffected) return Res.notFound();
+
+    return Res.json({
+      status: 200,
+      data: "Pélicula borrada correctamente",
+    });
+  } catch (error) {
+    return Res.err();
+  }
 };
